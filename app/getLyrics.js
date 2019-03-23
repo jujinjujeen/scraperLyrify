20011e5a451f54915ac4bb99a58dfbe4e11b6db8
Евгений Диденко <eugene.d@reply.io>
2019-03-23 16:09:31 +0200

    caching and fetching from cache added

--- app/getLyrics.js
+++ app/getLyrics.js
@@ -4,21 +4,31 @@ const raceSuccess = require('./raceSuccess');
 const {
     getLyricsGenius,
     getLyricsGoogle,
-    getLyricsMusixmatch
+    getLyricsMusixmatch,
+    getLyricsFromDb,
+    setLyricsToDb
 } = require('./lyricsHelpers');
+
 
 const getLyrics = async (song) => {
     try {
-        const browser = await puppeteer.launch();
-        const text = await raceSuccess([
-            getLyricsGenius(browser, song),
-            getLyricsGoogle(browser, song),
-            getLyricsMusixmatch(browser, song)
-        ]);
-        await browser.close();
-        return text;
+        const lyrics = await getLyricsFromDb(song);
+        if (!lyrics) {
+            const browser = await puppeteer.launch();
+            const { text, type } = await raceSuccess([
+                getLyricsGenius(browser, song),
+                getLyricsGoogle(browser, song),
+                getLyricsMusixmatch(browser, song)
+            ]);
+            console.log(`got lyrics from ${type}`);
+            setLyricsToDb(song, text);
+            await browser.close();
+            return text;
+        }
+
+        return lyrics;
     } catch (e) {
-        console.log(e)
+        console.log('Problem occured fetching lyrics:', e)
     }
 }
 
